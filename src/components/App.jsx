895271b0f6d00c039a13f46e969b05024b38605f
Andrey Svyrskyi <akkad1v@gmail.com>
2024-03-05 05:05:59 +0200

    'update'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,7 @@ export default function App() {
     <div>
       <h1>Best selling</h1>
 
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
+      <Product name="Tacos With Lime" price={10.99} />
       <Product
         name="Fries and Burger"
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
