b806a282fa813adfff018b295a108692a1027df1
Andrey Svyrskyi <akkad1v@gmail.com>
2024-03-15 01:11:25 +0200

    'update'

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,26 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+// import { useState } from "react";
+// import Description from "./components/Description/Description";
+// import Options from "./components/Options/Options";
+// import Feedback from "./components/Feedback/Feedback";
+// import "./App.css";
+
+// // const feedbackType = { good: 0, neutral: 0, bad: 0 };
+
+// function App() {
+//   const updateFeedback = (feedbackType) => {
+//     console.log("feedbackType: ", feedbackType);
+//   };
+
+//   return (
+//     <div>
+//       <Description />
+//       <Options updateFeedback={updateFeedback} />
+//       <Feedback />
+//     </div>
+//   );
+// }
+
+// export default App;
